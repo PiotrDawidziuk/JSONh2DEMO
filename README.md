b9b7c8e9846f539af25f0dc822810656d9e7c209
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-05-14 14:27:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JSONh2DEMO
---
+++ src/main/java/pl/piotrdawidziuk/JsoNh2DemoApplication.java
@@ -0,0 +1,13 @@
+package pl.piotrdawidziuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoNh2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoNh2DemoApplication.class, args);
+	}
+
+}
